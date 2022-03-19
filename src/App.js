c81228c8c5e417a1b39e2383020e869b7e82835e
Thomas Maynard <90271276+Thomas4897@users.noreply.github.com>
2022-03-19 15:38:22 +0800

    Adding handleOnClick function

--- src/App.js
+++ src/App.js
@@ -2,25 +2,142 @@ import React, { Component } from "react";
 import "./App.css";
 
 export class App extends Component {
+  state = {
+    playerTurn: true,
+    // board: [
+    //   {
+    //     name: "top-left",
+    //     empty: true,
+    //   },
+    //   {
+    //     name: "top-middle",
+    //     empty: true,
+    //   },
+    //   {
+    //     name: "top-right",
+    //     empty: true,
+    //   },
+    // ],
+    topLeft: { empty: true, value: "" },
+    topMiddle: { empty: true, value: "" },
+    topRight: { empty: true, value: "" },
+    middleLeft: { empty: true, value: "" },
+    middleMiddle: { empty: true, value: "" },
+    middleRight: { empty: true, value: "" },
+    bottomLeft: { empty: true, value: "" },
+    bottomMiddle: { empty: true, value: "" },
+    bottomRight: { empty: true, value: "" },
+  };
+
+  handleOnClick = (event) => {
+    const { playerTurn } = this.state;
+    let symbol = playerTurn === true ? "X" : "O";
+
+    if (this.state[event.target.id].empty === true) {
+      this.setState(
+        {
+          playerTurn: !playerTurn,
+          [event.target.id]: {
+            empty: false,
+            value: symbol,
+          },
+        },
+        () => console.log("handleOnClick:", this.state)
+      );
+    }
+  };
+
   render() {
+    const {
+      topLeft,
+      topMiddle,
+      topRight,
+      middleLeft,
+      middleMiddle,
+      middleRight,
+      bottomLeft,
+      bottomMiddle,
+      bottomRight,
+    } = this.state;
+
     return (
       <div className="App">
         <h1>Tic Tac Toe</h1>
-        <div className="board">
-          <div className="top-board">
-            <div id="top-left">top-left</div>
-            <div id="top-middle">top-middle</div>
-            <div id="top-right">top-right</div>
+        <h3>
+          {this.state.playerTurn === true
+            ? "(Player 1's Turn!)"
+            : "(Player 2's Turn!)"}
+        </h3>
+        <br />
+        <div name="mainBoard" className="board">
+          <div name="board" className="board-sections" id="top-board">
+            <div
+              className="board-squares"
+              id="topLeft"
+              onClick={this.handleOnClick}
+            >
+              {topLeft.value}
+            </div>
+            <div
+              className="board-squares"
+              id="topMiddle"
+              onClick={this.handleOnClick}
+            >
+              {topMiddle.value}
+            </div>
+            <div
+              className="board-squares"
+              id="topRight"
+              onClick={this.handleOnClick}
+            >
+              {topRight.value}
+            </div>
           </div>
-          <div className="middle-board">
-            <div id="middle-left">middle-left</div>
-            <div id="middle-middle">middle-middle</div>
-            <div id="middle-right">middle-right</div>
+          <div className="board-sections" id="middle-board">
+            <div
+              className="board-squares"
+              id="middleLeft"
+              onClick={this.handleOnClick}
+            >
+              {middleLeft.value}
+            </div>
+            <div
+              className="board-squares"
+              id="middleMiddle"
+              onClick={this.handleOnClick}
+            >
+              {middleMiddle.value}
+            </div>
+            <div
+              className="board-squares"
+              id="middleRight"
+              onClick={this.handleOnClick}
+            >
+              {middleRight.value}
+            </div>
           </div>
-          <div className="bottom-board">
-            <div id="bottom-left">bottom-left</div>
-            <div id="bottom-middle">bottom-middle</div>
-            <div id="bottom-right">bottom-right</div>
+          <div className="board-sections" id="bottom-board">
+            <div
+              className="board-squares"
+              id="bottomLeft"
+              onClick={this.handleOnClick}
+            >
+              {bottomLeft.value}
+            </div>
+            <div
+              className="board-squares"
+              id="bottomMiddle"
+              onClick={this.handleOnClick}
+            >
+              {bottomMiddle.value}
+            </div>
+            <div
+              className="board-squares"
+              id="bottomRight"
+              onClick={this.handleOnClick}
+            >
+              {bottomRight.value}
+            </div>
           </div>
         </div>
       </div>
